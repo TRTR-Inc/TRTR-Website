2b43c2f0876e016622c3e283c96d5d746357614e
TRTR-Inc <thatikondasaikumar712@gmail.com>
2025-02-05 20:26:55 -0500

    Fix: Ensure aws-exports.js is generated

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,15 +1,6 @@
-const awsmobile = {
-    aws_project_region: "us-west-2",
-    aws_cognito_identity_pool_id: "us-west-2:c88c24a1-bc96-44cc-9835-e1f6b49da74f",
-    aws_cognito_region: "us-west-2",
-    aws_user_pools_id: "us-west-2_hy3tICmgf",
-    aws_user_pools_web_client_id: "2v4ft29the2kj0c1v7dn9nnfqk",
-    oauth: {
-        domain: "trtr.auth.us-west-2.amazoncognito.com",
-        scope: ["email", "openid"],
-        redirectSignIn: "https://www.trtr-inc.com/",
-        redirectSignOut: "https://www.trtr-inc.com/",
-        responseType: "code"
-    },
+export default {
+aws_project_region:'us-west-2',
+aws_cognito_identity_pool_id:'us-west-2:c88c42a1-b9c6-44cc-9835-e1f6b49da74f',
+aws_user_pools_id:'us-west-2_h3ytICmgf',
+aws_user_pools_web_client_id:'2v4ft29the2kj0c1v7dn9nnfqk'
 };
-export default awsmobile;
