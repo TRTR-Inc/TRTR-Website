41b106a59e2ae493e6df2c2ecc0ced592aa57560
TRTR-Inc <thatikondasaikumar712@gmail.com>
2025-02-05 20:52:15 -0500

    Fix: Ensure aws-exports.js is generated

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -2,5 +2,5 @@ export default {
 aws_project_region:'us-west-2',
 aws_cognito_identity_pool_id:'us-west-2:c88c42a1-b9c6-44cc-9835-e1f6b49da74f',
 aws_user_pools_id:'us-west-2_h3ytICmgf',
-aws_user_pools_web_client_id:'2v4ft29the2kj0c1v7dn9nnfqk'
+aws_user_pools_web_client_id:'aosek5nn57cmbu5sk9fmjhg2b'
 };
